e2ba2d338d31530d0de91ec101a074edf24a4fdf
sahariar8 <sahariaralam8@gmail.com>
2025-03-03 02:04:00 +0600

    Frontend- User Dashboard mastering and logout implementation

--- routes/web.php
+++ routes/web.php
@@ -4,14 +4,13 @@ use App\Http\Controllers\Backend\AdminController;
 use App\Http\Controllers\Backend\VendorController;
 use App\Http\Controllers\Frontend\HomeController;
 use App\Http\Controllers\ProfileController;
+use App\Http\Controllers\UserDashboardController;
 use Illuminate\Support\Facades\Route;
 
 
 Route::get('/',[HomeController::class,'index'])->name('home');
 
-Route::get('/dashboard', function () {
-    return view('dashboard');
-})->middleware(['auth', 'verified'])->name('dashboard');
+
 
 Route::middleware('auth')->group(function () {
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
@@ -22,3 +21,8 @@ Route::middleware('auth')->group(function () {
 require __DIR__.'/auth.php';
 
 Route::get('admin/login',[AdminController::class,'login'])->name('admin.login');
+//user dashboard
+Route::group(['prefix'=>'user','as'=>'admin','middleware'=>['auth','verified']],function(){
+    Route::get('dashboard',[UserDashboardController::class,'index'])->name('dashboard');
+    
+});
