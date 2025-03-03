7186d74011adb49d44903e5a8ca7bbfdbfd7d877
sahariar8 <sahariaralam8@gmail.com>
2025-03-04 00:00:25 +0600

    Backend: slider page design

--- routes/admin.php
+++ routes/admin.php
@@ -2,6 +2,7 @@
 
 use App\Http\Controllers\Backend\AdminController;
 use App\Http\Controllers\Backend\ProfileController;
+use App\Http\Controllers\Backend\SliderController;
 use Illuminate\Support\Facades\Route;
 
 /**  Admin Route */
@@ -10,3 +11,8 @@ Route::get('dashboard',[AdminController::class,'adminDashboard'])->name('dashboa
 Route::get('profile',[ProfileController::class,'index'])->name('profile');
 Route::post('profile/update',[ProfileController::class,'profileUpdate'])->name('profile.update');
 Route::post('password/update',[ProfileController::class,'passwordUpdate'])->name('password.update');
+
+
+/** Slider Route */
+
+Route::resource('slider',SliderController::class);
